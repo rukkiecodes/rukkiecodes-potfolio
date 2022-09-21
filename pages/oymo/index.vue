c317070e1a8134217581a693135822b532dbb0e3
Terry Amagboro <rukkiecodes@gmail.com>
2022-09-21 21:34:30 +0100

    done with oymo route

--- pages/oymo/index.vue
+++ pages/oymo/index.vue
@@ -8,7 +8,7 @@
     >
       <p class="font-weight-bold text-h5 text-sm-h4 text-md-h3 text-lg-h2">work(<span class="deep-purple--text text--accent-4 font-weight-light">'Oymo'</span>)</p>
       <p class="deep-purple--text text--accent-4">// Head of Project, backend and Frontend</p>
-      <p class="deep-purple--text text--accent-4">// Oymo v1</p>
+      <p class="deep-purple--text text--accent-4">// Oymo v1 <v-btn depressed outlined rounded color="white" class="text-capitalize" href="https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40rukkiecodes/oymo-26667225898543a68cdb2140e4db93cb-signed.apk">Download Oymo</v-btn></p>
       <p class="font-weight-bold text-h5 text-sm-h4 text-md-h3 text-lg-h2 deep-purple--text text--accent-4">The dating giant</p>
       <p class="blue-grey--text text--darken-1">
         As you may know, they are a lot of dating apps and meetup apps. That is expensive and not engaging enough to keep users entertained.😥 This is where Oymo comes in.<br>
