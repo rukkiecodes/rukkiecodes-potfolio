fd5a51428dbc828e114c371d25327775369e3078
Terry Amagboro <rukkiecodes@gmail.com>
2022-09-21 23:21:05 +0100

    done with bluezone

--- pages/blueZoneAdmin/index.vue
+++ pages/blueZoneAdmin/index.vue
@@ -1,6 +1,25 @@
 <template>
   <v-container style="margin-top: 40vh;">
-    blueZoneAdmin
+    <v-row class="my-16">
+      <v-col cols="12">
+        <p class="blue-grey--text text--darken-1">// More...</p>
+      </v-col>
+      <v-toolbar
+        color="transparent"
+        dense
+        flat
+      >
+        <nuxt-link
+          to="/blueZone"
+          class="deep-purple--text text--accent-4 text-decoration-none"
+        >BlueZone Finance</nuxt-link>
+        <v-spacer />
+        <nuxt-link
+          to="/maystro"
+          class="deep-purple--text text--accent-4 text-decoration-none"
+        >Maystro</nuxt-link>
+      </v-toolbar>
+    </v-row>
   </v-container>
 </template>
 
