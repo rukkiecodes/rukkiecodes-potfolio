816eb144be8f9b3b4016f83e7be5072e44f243fa
rukkiecodes <rukkiecodes@gmail.com>
2025-07-29 14:29:34 +0100

    add recido

--- src/pages/[project].vue
+++ src/pages/[project].vue
@@ -1,59 +1,49 @@
 <template>
-  <v-container style="margin-top: 40vh;">
+  <v-container style="margin-top: 40vh">
     <v-row>
       <v-col cols="12">
-        <v-card
-          rounded="lg"
-          class="pa-0 ma-0"
-          color="transparent"
-          flat
-        >
-          <p class="font-weight-bold text-h5 text-sm-h4 text-md-h3 text-lg-h2">work(<span
-            class="text-indigo-accent-4 font-weight-light">'{{ projectData?.title }}'</span>)</p>
-          <p class="font-weight-bold text-h6 text-indigo-accent-4 mb-10">{{
-              projectData?.subtitle
-            }}</p>
+        <v-card rounded="lg" class="pa-0 ma-0" color="transparent" flat>
+          <p class="font-weight-bold text-h5 text-sm-h4 text-md-h3 text-lg-h2">
+            work(<span class="text-indigo-accent-4 font-weight-light"
+              >'{{ projectData?.title }}'</span
+            >)
+          </p>
+          <p class="font-weight-bold text-h6 text-indigo-accent-4 mb-10">
+            {{ projectData?.subtitle }}
+          </p>
         </v-card>
       </v-col>
 
       <v-col cols="12">
-        <v-card
-          rounded="lg"
-          class="pa-0 ma-0"
-          color="transparent"
-          flat
-        >
-          <v-img
-            min-height="550"
-            :src="projectData?.banner"
-          />
-        </v-card>
+        <v-img max-height="550" :src="projectData?.banner" rounded="lg" />
       </v-col>
     </v-row>
 
-
-    <v-card
-      max-width="750"
-      flat
-      color="transparent"
-      class="mb-16"
-    >
-      <p class="font-weight-bold text-h6 text-sm-h5 text-md-h4 text-lg-h3 mt-16 mb-5">Overview</p>
+    <v-card max-width="750" flat color="transparent" class="mb-16">
+      <p
+        class="font-weight-bold text-h6 text-sm-h5 text-md-h4 text-lg-h3 mt-16 mb-5"
+      >
+        Overview
+      </p>
       <p
         v-for="(line, index) in projectData?.overview"
         :key="index"
         class="text-grey-lighten-1"
       >
-        {{ line }}<br><br>
+        {{ line }}<br /><br />
       </p>
 
-      <p class="font-weight-bold text-h6 text-sm-h5 text-md-h4 text-lg-h3 mt-16 mb-5">About {{ projectData?.title }}</p>
+      <p
+        class="font-weight-bold text-h6 text-sm-h5 text-md-h4 text-lg-h3 mt-16 mb-5"
+      >
+        About {{ projectData?.title }}
+      </p>
       <p
         v-for="(line, index) in projectData?.about"
         :key="index"
         class="text-grey-lighten-1"
       >
-        {{ line }}<br><br>
+        {{ line }}<br /><br />
       </p>
     </v-card>
 
@@ -66,16 +56,8 @@
         v-for="(image, index) in projectData?.images"
         :key="index"
       >
-        <v-card
-          rounded="lg"
-          class="pa-0 ma-0"
-          flat
-          color="transparent"
-        >
-          <v-img
-            min-height="550"
-            :src="image"
-          />
+        <v-card rounded="lg" class="pa-0 ma-0" flat color="transparent">
+          <v-img min-height="550" :src="image" />
         </v-card>
       </v-col>
     </v-row>
@@ -83,24 +65,24 @@
 </template>
 
 <script>
-import {useAppStore} from "@/stores/app.js";
+import { useAppStore } from "@/stores/app.js";
 
 export default {
   data: () => ({
-    projectData: null
+    projectData: null,
   }),
 
   setup() {
-    const {projects} = useAppStore()
+    const { projects } = useAppStore();
     return {
-      projects
-    }
+      projects,
+    };
   },
 
   mounted() {
-    const projectName = this.$route.params?.project
-    const project = this.projects.find(obj => obj?.title == projectName)
-    this.projectData = project
+    const projectName = this.$route.params?.project;
+    const project = this.projects.find((obj) => obj?.title == projectName);
+    this.projectData = project;
   },
-}
+};
 </script>
