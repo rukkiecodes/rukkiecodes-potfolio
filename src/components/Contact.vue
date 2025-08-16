31b5eaa2dc649fb6726c0d52583a44ce36f4d7a1
rukkiecodes <rukkiecodes@gmail.com>
2025-08-16 01:42:58 +0100

    add blob converter to kitchen

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,7 +4,14 @@
     rounded="lg"
     color="indigo accent-4"
     size="small"
+    @click="contactMe"
   >
     Contact me
   </v-btn>
 </template>
+
+<script setup>
+const contactMe = () => {
+  window.open('mailto:rukkiecodes@gmail.com')
+}
+</script>
